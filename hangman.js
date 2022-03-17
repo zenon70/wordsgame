e9bf447d82d52a54ecdcf465f65efdb142856157
zenon70 <97974128+zenon70@users.noreply.github.com>
2022-03-18 02:53:50 +0600

    Update hangman.js

--- hangman.js
+++ hangman.js
@@ -43,7 +43,7 @@ function generateButtons() {
   let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
     `
       <button
-        class="btn btn-lg btn-primary m-2"
+        class="btn btn-lg btn-warning m-2"
         id='` + letter + `'
         onClick="handleGuess('` + letter + `')"
       >
